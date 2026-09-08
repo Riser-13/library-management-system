19ee2ceb773d8b5fda672687dabb16a761c05392
Harshit Kumar Singh <harshit.singh5726@gmail.com>
2026-09-08 17:03:13 +0530

    Add initial library management system

---
+++ LibraryManagementSystem.java
@@ -0,0 +1,9 @@
+public class LibraryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("================================");
+        System.out.println("   LIBRARY MANAGEMENT SYSTEM");
+        System.out.println("================================");
+        System.out.println("Welcome to the Library!");
+    }
+}
